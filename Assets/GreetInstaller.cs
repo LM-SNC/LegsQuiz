fbad1e67852bf1949214682c65e4abdaa8bb7523
ART <HerGovnoZalupa@mailkavo.ruu>
2023-09-25 01:55:35 +0300

    Add

--- Assets/GreetInstaller.cs
+++ Assets/GreetInstaller.cs
@@ -1,4 +1,8 @@
+using FileTransfer;
+using Google.Protobuf;
+using ProtoBuf;
 using Reflex.Core;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GreetInstaller : MonoBehaviour, IInstaller
@@ -6,6 +10,10 @@ public class GreetInstaller : MonoBehaviour, IInstaller
     public void InstallBindings(ContainerDescriptor descriptor)
     {
        // descriptor.AddInstance("World");
-        descriptor.AddSingleton(typeof(Greeter), typeof(IStartable)); // IStartable will force it to be constructed on container build
+       var testObject = new GetFilesRequest();
+
+       testObject.ToByteArray();
+       
+       descriptor.AddSingleton(typeof(Greeter), typeof(IStartable)); // IStartable will force it to be constructed on container build
     }
 }
--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -6,10 +6,10 @@ public class Loader : MonoBehaviour
 {
     private void Start()
     {
-        ReflexSceneManager.LoadScene("Greet", LoadSceneMode.Single, builder =>
-        {
-            // This deferred descriptor will run just before Greet.unity SceneScope installers
-           // builder.AddInstance("beautiful");
-        });
+        // ReflexSceneManager.LoadScene("Greet", LoadSceneMode.Single, builder =>
+        // {
+        //     // This deferred descriptor will run just before Greet.unity SceneScope installers
+        //    // builder.AddInstance("beautiful");
+        // });
     }
 }
