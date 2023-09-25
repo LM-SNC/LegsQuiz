961da3942efc188254932b860d5b0239753d50b7
ART <HerGovnoZalupa@mailkavo.ruu>
2023-09-25 10:19:40 +0300

    Edit leader board and main menu button

--- Assets/GreetInstaller.cs
+++ Assets/GreetInstaller.cs
@@ -1,8 +1,4 @@
-using FileTransfer;
-using Google.Protobuf;
-using ProtoBuf;
 using Reflex.Core;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GreetInstaller : MonoBehaviour, IInstaller
@@ -10,9 +6,8 @@ public class GreetInstaller : MonoBehaviour, IInstaller
     public void InstallBindings(ContainerDescriptor descriptor)
     {
        // descriptor.AddInstance("World");
-       var testObject = new GetFilesRequest();
 
-       testObject.ToByteArray();
+
        
        descriptor.AddSingleton(typeof(Greeter), typeof(IStartable)); // IStartable will force it to be constructed on container build
     }
